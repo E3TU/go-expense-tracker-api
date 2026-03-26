3435f0b4a1f5cafe5ca5d44c1e47581c32476c74
Eetu <99178022+E3TU@users.noreply.github.com>
2026-03-26 22:16:53 +0200

    Changed id from int to string

--- models/expense.go
+++ models/expense.go
@@ -1,7 +1,7 @@
 package models
 
 type Expense struct {
-	ID          int     `json:"id"`
+	ID          string  `json:"id"`
 	Description string  `json:"description"`
 	Amount      float64 `json:"amount"`
 }
