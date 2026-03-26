868f93f4b20dbedbe63b7dbec2dd8fee38daaa23
Eetu <99178022+E3TU@users.noreply.github.com>
2026-03-26 22:17:22 +0200

    Crud functions

--- handlers/handler.go
+++ handlers/handler.go
@@ -5,10 +5,60 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
 
 var expenses = []models.Expense{}
 
 func GetExpenses(c *gin.Context) {
 	c.JSON(http.StatusOK, expenses)
+}
+
+func CreateExpense(c *gin.Context) {
+	var input models.Expense
+	if err := c.BindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var guid = xid.New()
+
+	input.ID = guid.String()
+	expenses = append(expenses, input)
+
+	c.JSON(http.StatusCreated, input)
+}
+
+func UpdateExpense(c *gin.Context) {
+	id := c.Param("id")
+	var input models.Expense
+	if err := c.BindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	for i, expense := range expenses {
+
+		if expense.ID == id {
+			expenses[i].Description = input.Description
+			expenses[i].Amount = input.Amount
+			c.JSON(http.StatusOK, expenses[i])
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, gin.H{"error": "expense not found"})
+}
+
+func DeleteExpense(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, expense := range expenses {
+
+		if expense.ID == id {
+			expenses = append(expenses[:i], expenses[i+1:]...)
+			c.JSON(http.StatusOK, gin.H{"message": "deleted"})
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, gin.H{"error": "expense not found"})
 }
